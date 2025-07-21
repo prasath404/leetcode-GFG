32238ef60d85ad33ae8268593012a07f36a0f6b5
Hari Prasath S <120077816+prasath404@users.noreply.github.com>
2025-07-22 00:11:46 +0530

    Time: 518 ms (56.47%) | Memory: 17.7 MB (15.29%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def makeFancyString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        
+        count=1
+        result=[]
+        for i in range(len(s)):
+            if i>0 and s[i]==s[i-1]:
+                count+=1
+            else:
+                count=1
+            if count<3:
+                result.append(s[i])
+        return ''.join(result)
