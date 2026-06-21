9744deba828a737b249870242d04783248b709ed
Hari Prasath S <120077816+prasath404@users.noreply.github.com>
2026-06-21 17:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (46.07%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int result = 0;
+        for(int i = 0;i<tickets.length;i++){
+            if(i<=k){
+                result = result + Math.min(tickets[i],tickets[k]);
+            }
+            else{
+                result = result + Math.min(tickets[i],tickets[k]-1);
+            }
+        }
+        return result;      
+    }
+}
